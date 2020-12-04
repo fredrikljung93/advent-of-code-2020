1ef1eed906adf11d3600e5fe9eb2b25071db02fe
Fredrik Ljung <fredrik.ljung@tellox.se>
2020-12-05 00:07:13 +0100

    Add Day 2 part 2

--- day2.py
+++ day2.py
@@ -4,10 +4,9 @@ import re
 path = 'puzzleinputs/day2.txt'
 
 lines = open(path, 'r').read().splitlines()
-linesCount = len(lines)
 
 
-def valid_password(line):
+def valid_password_1(line):
     splitted = re.split(' ', line.replace(':', ''))
     numbers = list(map(int, re.split('-', splitted[0])))
     char = splitted[1]
@@ -16,8 +15,21 @@ def valid_password(line):
     return numbers[0] <= count <= numbers[1]
 
 
+def valid_password_2(line):
+    splitted = re.split(' ', line.replace(':', ''))
+    numbers = list(map(int, re.split('-', splitted[0])))
+    char = splitted[1]
+    pw = splitted[2]
+    return (pw[numbers[0] - 1] == char) != (pw[numbers[1] - 1] == char)
+
+
 def part_one():
-    return sum(map(lambda l: 1 if valid_password(l) else 0, lines))
+    return sum(map(lambda l: 1 if valid_password_1(l) else 0, lines))
+
+
+def part_two():
+    return sum(map(lambda l: 1 if valid_password_2(l) else 0, lines))
 
 
 print("Part 1:", part_one())
+print("Part 2:", part_two())
