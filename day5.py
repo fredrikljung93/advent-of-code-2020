1de23a04fe9c41ad71ddfb0d0e101e95b8fd8bcb
Fredrik Ljung <fredrik.ljung@tellox.se>
2020-12-06 18:55:27 +0100

    Day 5 part two

--- day5.py
+++ day5.py
@@ -13,10 +13,19 @@ def helper(mi, ma, lower_half_char, s):
         return helper((mi + range_width // 2 + 1), ma, lower_half_char, s[1:])
 
 
-def seat_id(s):
-    return [helper(0, 127, 'F', s[0:7]) * 8 + helper(0, 7, 'L', s[-3:])]
+def seat(s): return [helper(0, 127, 'F', s[0:7]), helper(0, 7, 'L', s[-3:])]
 
 
-seat_ids = map(lambda l: seat_id(l), lines)
+def seat_id(s): return s[0] * 8 + s[1]
 
-print(max(seat_ids))
+
+taken_seats = list(map(lambda l: seat(l), lines))
+taken_seatids = list(map(seat_id, taken_seats))
+max_seat_id = max(taken_seatids)
+min_seat_id = min(taken_seatids)
+all_seats = list(range(min_seat_id, max_seat_id + 1))
+
+free_seats = [x for x in all_seats if x not in taken_seatids]
+
+print(max_seat_id)
+print(free_seats[0])
